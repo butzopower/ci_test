d3ebd5ff9ce4bca6d023c080308ebdd957576bca
Brian Butz and George Dean <pair+bbutz+gdean@pivotallabs.com>
2014-03-31 09:46:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CiTest::Application.config.secret_key_base = '5935e376c8a92253e1a18ed0e9b7ae941fb3e3d109e97c544af6e10d647085127c347e2c8d6a18cf766ec1918b31a054e835ddfed3a8e36b5a9848952d76d0f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CiTest::Application.config.session_store :cookie_store, key: '_ci_test_session'
